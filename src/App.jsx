d91de52a8b5c37c766922cae8ddbbcb3ecfa7cf4
KrishDave1 <116882111+KrishDave1@users.noreply.github.com>
2023-09-21 15:16:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,15 @@ import StarsCanvas from './components/canvas/Stars';
 import Login from './components/Login';
 import Register from './components/Register';
 import Payments from './components/Payments';
-import { useGlobalContext } from './context';
-import Profile from './components/Profile';
-import ForgotPassword from './components/ForgotPassword';
+import { useGlobalContext } from './context'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
-  const { showModal,currentUser } = useGlobalContext()
+  const { showModal } = useGlobalContext()
   return (
     <Router scrollRestoration="auto">
       <main>
         <Navbar />
-        {/* {currentUser && <div className="user-info"> <strong className="nav-user-name">Welcome, {currentUser}</strong></div>} */}
         {showModal && <Modal />}
         <Routes>
           <Route path='/' element={
@@ -53,11 +50,6 @@ export default function App() {
             </div>
           }>
           </Route>
-          <Route
-             path='/profile'
-             element={<Profile />}
-          >
-          </Route>
           <Route path='/login' element={
             <>
               <Login />
@@ -67,12 +59,6 @@ export default function App() {
           <Route path='/register' element={
             <>
               <Register />
-            </>
-          }>
-          </Route>
-          <Route path='/forgot-password' element={
-            <>
-              <ForgotPassword />
             </>
           }>
           </Route>
